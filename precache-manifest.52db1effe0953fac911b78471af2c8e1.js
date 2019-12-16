29ad664306f76b95887465995b7089118a0ddb7e
thevenine <seyeoyeniran2u@gmail.com>
2019-12-16 18:10:40 +0100

    Updates

---
+++ precache-manifest.52db1effe0953fac911b78471af2c8e1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73cd09d879bda8535bc2bd61c1623722",
+    "url": "/react-calc/index.html"
+  },
+  {
+    "revision": "23c3745ef48bf62f5a7d",
+    "url": "/react-calc/static/css/main.ad652649.chunk.css"
+  },
+  {
+    "revision": "a96d0ad715a43a57b7fa",
+    "url": "/react-calc/static/js/2.da22c0d2.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-calc/static/js/2.da22c0d2.chunk.js.LICENSE"
+  },
+  {
+    "revision": "23c3745ef48bf62f5a7d",
+    "url": "/react-calc/static/js/main.adbb0df3.chunk.js"
+  },
+  {
+    "revision": "d257b84c32a6dfe77ab5",
+    "url": "/react-calc/static/js/runtime-main.64ee69e0.js"
+  }
+]);
